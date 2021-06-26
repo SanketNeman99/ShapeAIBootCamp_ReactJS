785ac844ce9d16a5a5a6768b7c58841c54b5395f
SanketNeman99 <86418134+SanketNeman99@users.noreply.github.com>
2021-06-26 20:08:51 +0530

    Create Note.jsx

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React</h1>
+      <p>basic intro to both</p>
+    </div>
+  );
+}
+
+export default Info;
